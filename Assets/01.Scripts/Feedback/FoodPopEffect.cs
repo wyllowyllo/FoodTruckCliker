f8121ff408fdb00d32206390073eaf69a0eb340b
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2026-01-28 10:51:56 +0900

    chore : popeffect 스케일 조정 방식으로 변경

--- Assets/01.Scripts/Feedback/FoodPopEffect.cs
+++ Assets/01.Scripts/Feedback/FoodPopEffect.cs
@@ -24,62 +24,23 @@ namespace FoodTruckClicker.Feedback
         private float _fadeOutDuration = 0.5f;
 
         private Rigidbody _rigidbody;
-        private Renderer[] _renderers;
-        private Material[][] _materials;
+        private Vector3 _originalScale;
         private Coroutine _deactivateCoroutine;
-
-        private static readonly int SurfaceProperty = Shader.PropertyToID("_Surface");
-        private static readonly int BlendProperty = Shader.PropertyToID("_Blend");
-        private static readonly int SrcBlendProperty = Shader.PropertyToID("_SrcBlend");
-        private static readonly int DstBlendProperty = Shader.PropertyToID("_DstBlend");
-        private static readonly int ZWriteProperty = Shader.PropertyToID("_ZWrite");
-        private static readonly int BaseColorProperty = Shader.PropertyToID("_BaseColor");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
-            _renderers = GetComponentsInChildren<Renderer>();
-            InitializeMaterials();
-        }
-
-        private void InitializeMaterials()
-        {
-            _materials = new Material[_renderers.Length][];
-
-            for (int i = 0; i < _renderers.Length; i++)
-            {
-                Material[] originalMaterials = _renderers[i].materials;
-                _materials[i] = new Material[originalMaterials.Length];
-
-                for (int j = 0; j < originalMaterials.Length; j++)
-                {
-                    _materials[i][j] = originalMaterials[j];
-                    SetMaterialTransparent(_materials[i][j]);
-                }
-
-                _renderers[i].materials = _materials[i];
-            }
-        }
-
-        private void SetMaterialTransparent(Material material)
-        {
-            material.SetFloat(SurfaceProperty, 1f);
-            material.SetFloat(BlendProperty, 0f);
-            material.SetFloat(SrcBlendProperty, (float)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            material.SetFloat(DstBlendProperty, (float)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            material.SetFloat(ZWriteProperty, 0f);
-            material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-            material.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
+            _originalScale = transform.localScale;
         }
 
         public void Play(Vector3 startPosition)
         {
             transform.position = startPosition;
             transform.rotation = Random.rotation;
+            transform.localScale = _originalScale;
 
             ResetVelocity();
             ApplyPopForce();
-            SetAlpha(1f);
 
             if (_deactivateCoroutine != null)
             {
@@ -114,36 +75,24 @@ namespace FoodTruckClicker.Feedback
                 yield return new WaitForSeconds(waitTime);
             }
 
-            yield return FadeOut();
+            yield return ScaleDown();
             gameObject.SetActive(false);
         }
 
-        private IEnumerator FadeOut()
+        private IEnumerator ScaleDown()
         {
             float elapsed = 0f;
 
             while (elapsed < _fadeOutDuration)
             {
                 elapsed += Time.deltaTime;
-                float alpha = Mathf.Lerp(1f, 0f, elapsed / _fadeOutDuration);
-                SetAlpha(alpha);
+                float t = elapsed / _fadeOutDuration;
+                float scale = Mathf.Lerp(1f, 0f, t * t);
+                transform.localScale = _originalScale * scale;
                 yield return null;
             }
 
-            SetAlpha(0f);
-        }
-
-        private void SetAlpha(float alpha)
-        {
-            for (int i = 0; i < _renderers.Length; i++)
-            {
-                for (int j = 0; j < _materials[i].Length; j++)
-                {
-                    Color color = _materials[i][j].GetColor(BaseColorProperty);
-                    color.a = alpha;
-                    _materials[i][j].SetColor(BaseColorProperty, color);
-                }
-            }
+            transform.localScale = Vector3.zero;
         }
     }
 }
